dafef58a44ddaa8d4e8c4be65e7244357e5b5bb5
EX3QTE <bggergo@gmail.com>
2016-02-17 14:36:25 +0100

    Automatic table creation fix

--- lib/sequel-rack_throttle/Database.rb
+++ lib/sequel-rack_throttle/Database.rb
@@ -6,12 +6,8 @@ module Sequel
       alias_method :old_init, :initialize
 
       def initialize(args)
+        create_cache_table unless self.cache_table_exists
         old_init(args)
-
-        create_table? :throttle_cache do
-          String :key, primary_key: true
-          Float :value, default: 0
-        end
       end
 
       def cache_dataset
@@ -19,11 +15,21 @@ module Sequel
       end
 
       def get(key)
+        create_cache_table unless self.cache_table_exists
         (cache_dataset.filter(key: key).first[:value].to_f)
       end
 
       def set(key, value)
+        create_cache_table unless self.cache_table_exists
         unless cache_dataset.where(key: key).update(value: value) == 1 then cache_dataset.insert(key: key, value: value) end
+      end
+
+      def create_cache_table
+        create_table? :throttle_cache do
+          String :key, primary_key: true
+          Float :value, default: 0
+        end
+        self.cache_table_exists = true
       end
     end
   end

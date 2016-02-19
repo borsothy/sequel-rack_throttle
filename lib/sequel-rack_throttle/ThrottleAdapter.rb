887fd92a29b6ae5a0c51885f73cf22525ecddcf0
gborsothy <gergely.borsothy@cheppers.com>
2016-02-19 11:55:08 +0100

    Fix

--- lib/sequel-rack_throttle/ThrottleAdapter.rb
+++ lib/sequel-rack_throttle/ThrottleAdapter.rb
@@ -2,12 +2,17 @@ class ThrottleAdapter
   attr_accessor :db, :table
 
   def initialize(sequel_db, sequel_table = 'throttle_cache')
-    db = sequel_db
-    table = sequel_table
+    @db = sequel_db
+    @table = sequel_table
+
+    create_table? @table.to_sym do
+      String :key, primary_key: true
+      Float :value, default: 0
+    end
   end
 
   def dataset
-    db[table.to_sym]
+    @db[@table.to_sym]
   end
 
   def get(key)
